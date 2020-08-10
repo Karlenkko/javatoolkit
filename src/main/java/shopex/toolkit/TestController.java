743a8445409f21aa1ac9dafb7dd3a8a582b8a524
Karlenkko <48716383+Karlenkko@users.noreply.github.com>
2020-08-10 13:23:23 +0800

    Changes to be committed: 	new file:   .gitignore 	new file:   .mvn/wrapper/MavenWrapperDownloader.java 	new file:   .mvn/wrapper/maven-wrapper.jar 	new file:   .mvn/wrapper/maven-wrapper.properties 	new file:   mvnw 	new file:   mvnw.cmd 	new file:   nb-configuration.xml 	new file:   pom.xml 	new file:   src/main/java/shopex/toolkit/TestController.java 	new file:   src/main/java/shopex/toolkit/ToolkitApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/main/webapp/META-INF/context.xml 	new file:   src/main/webapp/index.html 	new file:   src/test/java/shopex/toolkit/ToolkitApplicationTests.java

---
+++ src/main/java/shopex/toolkit/TestController.java
@@ -0,0 +1,12 @@
+package shopex.toolkit;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/test")
+    public String hello() {
+        return "Hello Spring Boot";
+    }
+}
